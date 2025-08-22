baabea6fe141f23763ea956c0758f2ac55a23c1f
Abbatyya <abbaty48@gmail.com>
2025-08-22 10:36:57 +0100

    Implement check-grammer endpoint business logic.

--- router.plugin.js
+++ router.plugin.js
@@ -1,9 +1,29 @@
 import fastifyPlugin from "fastify-plugin";
+import Typojs from "typo-js";
 
 export default fastifyPlugin(async (fastify) => {
   fastify
     .get("/markdowns", async (req) => { })
-    .post("/check-grammer", async (req) => { })
+    .post("/check-grammer", async (req) => {
+      const dictionary = new Typojs("en_US");
+      const misspells = [];
+      const grammer = String(req.body.note);
+      const words = grammer.split(/\s/);
+
+      words.forEach((word, index) => {
+        if (!dictionary.check(word)) {
+          if (!misspells.some((misspell) => misspell.word === word)) {
+            const recommendations = dictionary.suggest(word);
+            misspells.push({ word, index, recommendations });
+          }
+        }
+      });
+
+      if (misspells.length > 0) {
+        return misspells;
+      }
+      return "You are clean.";
+    })
     .post("/save", async (req) => { })
     .post("/render", async (req) => { });
 });
